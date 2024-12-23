13648c84a0acbb5791d7b5dc08b4da239b394a67
Rémy Mondi <remy@mondi.dev>
2024-12-23 12:39:13 +0100

    First release

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const fs = require( 'node:fs' );
+
+try {
+  const data = fs.readFileSync( process.env.INPUT_FILE, 'utf8' );
+  
+  const words = [];
+
+  data.split( /\r?\n/ ).forEach( word => {
+
+    const formatWord = word.normalize( "NFD" ).replace( /[\u0300-\u036f]/g, '' ).toUpperCase();
+
+    words.push( formatWord );
+  } );
+
+  const jsonData = JSON.stringify( words, null, 2 );
+
+  fs.writeFileSync( process.env.OUTPUT_FILE, jsonData, "utf8", ( error, data ) => {
+    if ( error ) console.error( error );
+    else ( console.log( "Data written to file !" ) )
+  } );
+
+} catch ( error ) {
+  console.error( error );
+}
